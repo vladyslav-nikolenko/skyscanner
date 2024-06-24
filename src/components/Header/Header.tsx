7c9897f51d6bcf86f720df6837ff76e283abea3b
vladyslav-nikolenko <first00141@gmail.com>
2024-06-24 19:17:26 +0200

    add a link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,10 @@ const logo = require("../../../public/Logo") as string;
 export const Header = () => {
   return (
     <header className='header'>
-      <a href='/' className='logo logo--matgin-top'>
+      <a
+        href='https://vladyslav-nikolenko.github.io/skyscanner.github.io'
+        className='logo logo--matgin-top'
+      >
         <img className='logo__img' src={logo} alt='Logo' />
       </a>
     </header>
