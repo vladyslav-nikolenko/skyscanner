d5301067c1a3c9b8f9a03919601bfb33e5187ad2
vladyslav-nikolenko <first00141@gmail.com>
2024-06-24 19:29:25 +0200

    change link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className='header'>
       <a
-        href='https://vladyslav-nikolenko.github.io/skyscanner.github.io'
+        href='https://vladyslav-nikolenko.github.io/skyscanner.github.io/'
         className='logo logo--matgin-top'
       >
         <img className='logo__img' src={logo} alt='Logo' />
